ccaafe0db3a0acf64165f7984745ec61a45ee324
Richard Leek <richard@richardleek.com>
2026-02-14 11:47:39 +0100

    Migrate on app start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 echo "Initializing application..."
 
 # Do any necessary setup here, such as running migrations or seeding the database
+php vendor/bin/phinx migrate --configuration=phinx.php
 
 exec "$@"
