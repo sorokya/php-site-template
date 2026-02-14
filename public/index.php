c2efdf96c44f8f809309029649743fbef21dbf9d
Richard Leek <richard@richardleek.com>
2026-02-14 10:38:24 +0100

    Load dotenv in site

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 $url = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
 if (!is_string($url)) {
     http_response_code(400);
